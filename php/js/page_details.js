4d74e3cc448334d40901327df73194e77e148950
Yenny Cheung <yennycheung@hotmail.com>
2014-11-17 17:23:38 -0500

    added stars in reviews, took out comments

--- php/js/page_details.js
+++ php/js/page_details.js
@@ -1,7 +1,16 @@
 // Execute code without declaring global variables.
 $(function() {	
+	// For todays date;
+	Date.prototype.today = function () { 
+	    return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
+	}
 
-	var commentCount = 1;
+	// For the time now
+	Date.prototype.timeNow = function () {
+	     return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes();
+	}
+
+	//var commentCount = 1;
 	//star rating
 	$(".rating").starRating({
 		//minus: true // step minus button
@@ -18,8 +27,11 @@ $(function() {
 
 	$(".add-review-btn").click(function(){
 		var content = document.getElementById('reviewText').value;
-		//console.log(content);
-		var parsedDOMs = $.parseHTML("<div class='review'><p>"+content+"</p><div class='actions'><p class='action' id='upvote'>Upvote</p><p class='action comment' data-value = 0 >Comment</p></div><div class='comments'><div class='comment-textbox'><textarea rows='1' name='commentText' class='commentText' placeholder='Add a Comment...''></textarea><input type='submit' type='button' class='add-comment-btn' value='Comment'/></div></div></div>")
+		var rating = $('#ratingbigger').attr('data-val');
+		console.log(rating);
+		var newDate = new Date();
+		var datetime = "     Posted on " + newDate.today() + " at " + newDate.timeNow();
+		var parsedDOMs = $.parseHTML("<div class='review'> <p class = 'tag rating'>Rating: "+rating+"</p><p>"+content+"</p><div class='actions'><p class='action' id='upvote'>Upvote</p><p class='action' id='timestamp'>"+datetime+"</p></div></div>")
 		if (parsedDOMs.length < 1) {
 			return;
 		}
@@ -27,7 +39,7 @@ $(function() {
 		var newReviewDOM = parsedDOMs[0];
 		var newReviewJQuery = $(newReviewDOM);
 
-		newReviewJQuery.find(".comment").click(function() {
+		/*newReviewJQuery.find(".comment").click(function() {
 			newReviewJQuery.find(".comment-textbox").toggle();
 			newReviewJQuery.find(".comment-thread").toggle();
 		});
@@ -39,8 +51,7 @@ $(function() {
 			count++;
 		    newReviewJQuery.find(".comment").text("Comment ("+count+")");
 		    newReviewJQuery.find(".comment").attr("data-value", count);
-		});
-
+		});*/
 		$('#reviews').append(newReviewDOM);
 
 	});
