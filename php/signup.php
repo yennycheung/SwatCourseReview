2ba44a248106ae40ff002741e3d6a9bb1be57407
Yenny Cheung <yennycheung@hotmail.com>
2014-11-27 00:23:52 -0500

    fix signup and contacts css

--- php/signup.php
+++ php/signup.php
@@ -5,17 +5,14 @@
 	<div class="body">
 	<div class="loader"></div>	
 		
-		<section class="welcome bgParallax login" data-speed="2">
+		<section class="welcome bgParallax signup" data-speed="2">
 			<div class="wrapper">
 				<div class="contactscontainer">
 					<div class="top"> <span>Sign Up Now!</span> </div>
 					<div class="bottom">
 						<div class="form">
 						<form action="" method="post">
-							<input type="text" placeholder="First Name" name="forename" required><br>
-							<input type="text" placeholder="Last Name" name="surname" required><br>
 						    <input type="email" placeholder="Email Address" name="email" required><br>
-							<input type="text" placeholder="Graduation Year" name="gradyear" required><br>
 							<input type="password" placeholder="Password" name="password" required><br>
 							<input value="Submit" type="submit" >
 						</form>
