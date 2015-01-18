496f4a901b0fac69513a15b91dbfa8b6cb575009
Yenny Cheung <yennycheung@hotmail.com>
2015-01-18 02:38:08 +0100

    changed courses reviewed number

--- php/js/page_index.js
+++ php/js/page_index.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function() {
 
     // Number increasing when scroll.
     var start = 0, 
-    max = 30,
+    max = 33,
     scrollPos = $(window).scrollTop();
     $(window).scroll(function() {  
         if (scrollPos > 550 && scrollPos < 1655 && start < max) {
