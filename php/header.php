13ad73586da09c87e37b31515060de25b60c90ab
EC2 Default User <ec2-user@ip-172-31-39-244.us-west-2.compute.internal>
2015-01-24 17:03:16 +0000

    changing header again to swarthmore course review

--- php/header.php
+++ php/header.php
@@ -3,7 +3,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
   	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
     
-	<title>SwatCoRe - Swat Course Review</title>
+	<title>SwatCoRe - Swarthmore Course Review</title>
 
 	<!-- SASS INCLUDES -->
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
