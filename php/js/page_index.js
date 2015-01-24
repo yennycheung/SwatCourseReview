eaa4c82b535848fe198313ed8a78ca0b28eb88cd
EC2 Default User <ec2-user@ip-172-31-39-244.us-west-2.compute.internal>
2015-01-24 15:22:39 +0000

    updating course number

--- php/js/page_index.js
+++ php/js/page_index.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function() {
 
     // Number increasing when scroll.
     var start = 0, 
-    max = 45,
+    max = 52,
     scrollPos = $(window).scrollTop();
     $(window).scroll(function() {  
         if (scrollPos > 550 && scrollPos < 1655 && start < max) {
